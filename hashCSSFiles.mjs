e991fe38427b28605f73db8ffbb8d013211a2656
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2022-11-08 17:33:26 +0000

    feat: add compile into WP build for development

--- hashCSSFiles.mjs
+++ hashCSSFiles.mjs
@@ -1,6 +1,7 @@
 import crypto from "crypto";
 import { readFileSync, renameSync } from "node:fs";
 import glob from "glob";
+import { resolve } from "node:path";
 
 glob(`${process.cwd()}/build/**/*.css`, {}, function (error, matches) {
 	if (error) {
@@ -19,3 +20,28 @@ glob(`${process.cwd()}/build/**/*.css`, {}, function (error, matches) {
 	}
 	return;
 });
+
+glob(
+	`${resolve(
+		process.cwd(),
+		"../snap/public/wp-content/plugins/snap-blocks/build"
+	)}/**/*.css`,
+	{},
+	function (error, matches) {
+		if (error) {
+			throw error;
+		}
+		for (let match of matches) {
+			const fileBuffer = readFileSync(match);
+			const contentHash = crypto.createHash("shake256", { outputLength: 10 });
+			contentHash.update(fileBuffer);
+			const fileMatchArray = match.split("/");
+			const newFileName = fileMatchArray
+				.pop()
+				.replace(".css", `.${contentHash.digest("hex")}.css`);
+
+			renameSync(match, `${fileMatchArray.join("/")}/${newFileName}`);
+		}
+		return;
+	}
+);
