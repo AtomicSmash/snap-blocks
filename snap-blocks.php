7706000dff5ddc3e4913d78848d098858040e442
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2022-11-18 12:33:55 +0000

    Fix: update plugin header comment

--- snap-blocks.php
+++ snap-blocks.php
@@ -5,7 +5,8 @@
  * Requires at least: 5.9
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Mikey Binns
+ * Author:            Atomic Smash
+ * Author URI:        https://www.atomicsmash.co.uk/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       block-test
