6de5bd0666eea163a4d6b04ee871f65c3a13b452
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2023-01-19 10:19:50 +0000

    feat: remove tailwind

--- src/blocks/accordion-group/tailwind.block.config.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const tailwindConfig = require("../../../tailwind.config.cjs");
-module.exports = {
-	content: ["./**/save.{js,ts,jsx,tsx}"],
-	...tailwindConfig,
-};
