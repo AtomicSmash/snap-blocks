513f938d3763d2e12c444f8ea8130a6edaa1ae94
Mikey Binns <mikey.binns@atomicsmash.co.uk>
2023-03-30 15:41:42 +0100

    feat: add example preview content

--- src/blocks/accordion-group/versions/v1/index.ts
+++ src/blocks/accordion-group/versions/v1/index.ts
@@ -1,6 +1,7 @@
 import type {
 	BlockAttributes,
 	BlockSupports,
+	BlockExample,
 	InterpretAttributes,
 } from "@atomicsmash/blocks-helpers";
 import { Edit } from "./edit"; // Example of what to do if property has been updated in the new version.
@@ -35,9 +36,80 @@ export type InterpretedAttributes = InterpretAttributes<Attributes>;
 export const supports = {} as const satisfies BlockSupports;
 export type Supports = typeof supports;
 
+/**
+ * Block example
+ *
+ * This is the content that WordPress will use to preview your block in various situations.
+ *
+ * @see {@link https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#example}
+ */
+export const example = {
+	attributes: {
+		isMultiple: true,
+		accordionGroupId: "group-123",
+	},
+	innerBlocks: [
+		{
+			name: "snap-blocks/accordion",
+			attributes: {
+				isInitiallyOpen: true,
+				accordionId: "single-123",
+				headerContent: "Accordion title",
+				headerElement: "h3",
+			},
+			innerBlocks: [
+				{
+					name: "core/paragraph",
+					attributes: {
+						content:
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et eros eu felis.",
+					},
+				},
+			],
+		},
+		{
+			name: "snap-blocks/accordion",
+			attributes: {
+				isInitiallyOpen: true,
+				accordionId: "single-456",
+				headerContent: "Accordion title",
+				headerElement: "h3",
+			},
+			innerBlocks: [
+				{
+					name: "core/paragraph",
+					attributes: {
+						content:
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et eros eu felis.",
+					},
+				},
+			],
+		},
+		{
+			name: "snap-blocks/accordion",
+			attributes: {
+				isInitiallyOpen: true,
+				accordionId: "single-789",
+				headerContent: "Accordion title",
+				headerElement: "h3",
+			},
+			innerBlocks: [
+				{
+					name: "core/paragraph",
+					attributes: {
+						content:
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et eros eu felis.",
+					},
+				},
+			],
+		},
+	],
+} satisfies BlockExample<InterpretedAttributes>;
+
 export default {
 	attributes,
 	supports,
+	example,
 	edit: Edit,
 	save: Save,
 };
